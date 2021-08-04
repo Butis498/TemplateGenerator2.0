13885a4b3b3477df5dadae574ac3fb07da6288ca
Esteban Salazar <estebansalazar498@gmail.com>
2021-08-03 19:26:50 -0500

    auto generate folders

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-### Xlsx , Docx Multy replacement ###
-
-Program to replace words inside a .docx or an .xlsx doc filling with a csv file input , there is a dictionary of terms which can be actually replaced in the doc "Replacement_dictionary.txt" , this will generate multiple docs with diferent data replaced instead of the dictionary inside each doc or xls
-
-## Install openpyxl-2.5.11 ##
-
+## Xlsx , Docx Multy replacement ##
+ 
+Script to replace words inside a .docx or an .xlsx doc filling with a csv file input, there is a dictionary of terms which can be actually replaced in the "Replacement_dictionary.csv" document, this will generate multiple docs with different data inside the database which will also be in a csv format.
+ 
+### Install required packages ###
+ 
 ```console
-pip install openpyxl==2.5.11
+pip install -r requirements.txt
 ```
-other versions will not work properly
+ 
+### Generate a EXE file ###
+ 
+```console
+pyinstaller --onefile main.py
+```
+ 
+### Usage ###
+ 
+First we have to understand that there are 3 main components for the template generator, the first component is the "DB" folder which would be generated in the .exe file directory if not exists along with the second component which is the "Cards" directory, this directory is where all the the documents (docx or xlsx) will be, this documents have to be already filled with the corresponding information from the "Replacement_Dictionary.csv" file, the last component for the usage is the files in the "DB" directory, this directory contains 2 files that are essential for operation, the first file is the already mentioned: "Replacement_Dictionary.csv'' which contains the a dictionary in form of a csv file, with two columns: REPLACEMENT_KEYWORD and REPLACEMENT, REPLACEMENT_KEYWORD is the keyword used in the documents to fill the corresponding gap inside a document for example: "REPLACE_NAME" this will be the keyword to place on a document to replace the name of the database, the second column is the REPLACEMENT column this is the name of the column corresponding on the db for example: "Name" this mean that the script will fill N documents replacing REPLACE_NAME with the column "Name" of the database. The next file is the "db.csv" this file must also be in the "DB" directory, this file will contain the information of all the cases to fill, if the file has N rows the script will generate N x (Number of Cards in the Cards directory).
+ 
+#### IMPORTANT ####
+ 
+For this script to work is very important to maintain a very clean information, this means that there should not be any type of spaces (tabs, spaces, enter, etc..) on the beginning or the end of the columns name nor in the Replacement_dictionary file, if there is any type of space there will be conflicts which will cause the script to stop working, tips to avoid:
+ 
+- Avoid spaces in column names.
+- Avoid spaces in the dictionary file.
+- Avoid special characters like commas or accents in column names.
+- Avoid special characters like commas or accents in dictionary files.
+- IMPORTANT DO NOT USE ENTER inside any cell of the DB.csv file
+- The file names are always the same: "db.csv" and "Replacement_Dictionary.csv"
+ 
+#### Running the Script ####
+ 
+Once we have the 3 components previously mention (DB,Cards,2 DB files) we can now run the script, we can simply double click the script and it will automatically generate a directory by the name of Generated, this directory will contain all the filled documents numbered with the number of row in the db, in case the directory was not created this means that there is an error inside the dictionaries, try following the previous recommendations to check for errors in the dictionaries. 
--- TempleteGenerator.py
+++ TempleteGenerator.py
@@ -4,8 +4,13 @@ from docx import Document
 from openpyxl import load_workbook
 from pathlib import Path
 
+
+## Class to replace content of a csv with keywords in a document
 class TempleteGenerator():
 
+    ## Constructor to open required files and create required directories
+    ## Params: none
+    ## Returns: none
     def __init__(self):
         self.required_files = ['Replacement_Dictionary.csv','db.csv']
         DB_folder_name = 'DB'
@@ -14,7 +19,7 @@ class TempleteGenerator():
         if not os.path.isdir(directory):
             os.mkdir(directory)
 
-    
+        ## Validate that the requiered files exists, if they dont exists they will be automatically generated
         try:   
             self.validate_files_required(self.required_files,directory)
         except FileExistsError as e:
@@ -25,7 +30,8 @@ class TempleteGenerator():
 
         self.df =pd.read_csv(os.path.join(directory,'Replacement_Dictionary.csv'))
 
-    
+
+        ## Generate a json with the data of the db and the replacement dictionary
         self.json_data = {}
         for _, row in self.df.iterrows():
             self.json_data[row['REPLACE_KEYWORD']] = row['REPLACEMENT']
@@ -35,7 +41,7 @@ class TempleteGenerator():
 
 
         self.db =pd.read_csv(os.path.join(directory,'db.csv')) 
-        self.file = open(os.path.join(directory,"db.csv"))
+        self.file = open(os.path.join(directory,"db.csv"),encoding='utf8')
         self.data_count = len(self.file.readlines()) - 1
         self.file.close()
         self.Generated_folder_name = "\\Generated"
@@ -46,6 +52,11 @@ class TempleteGenerator():
             os.mkdir(os.getcwd() + self.Generated_folder_name)
 
 
+    ## Replace the content of a parograph depending on the key and some data to replace
+    ## Params: parograph= the text with the key words to be replaces
+    ##         key = the key which will be replaced
+    ##         data_to_replace = the data obtained from the db to replace instead of the key
+    ## Returns: the parograph with the replaced keys 
     def replacedParograph(self , parograph , key , data_to_replace):
         try:
             replaced = parograph.replace(key , data_to_replace)
@@ -54,6 +65,11 @@ class TempleteGenerator():
 
         return replaced
 
+
+    ## Function to validate if the files are inside the DB directory
+    ## Params: file_list = list of files requied to run the script
+    ##         directory = the directory where the files should be
+    ## Returns: none
     def validate_files_required(self,file_list,directory):
 
         for file in file_list:
@@ -61,10 +77,17 @@ class TempleteGenerator():
                 raise FileExistsError('Missing files for operation')
 
 
-    
+    ## Function to get the data from the db
+    ## Params: i : the index to get the information from the db
+    ##         key: the key to use to see which column to use in the db
+    ## Return: the data required depending on the key
     def get_Data(self , i , key):
         return str(self.db[self.json_data[key]].iloc[i])
 
+
+    # Function to verify if a value is a float
+    ## Params: value = value to check if float
+    ## Returns: bool with the answere if the value is a float
     def isfloat(self , value):
         try:
             float(value)
@@ -72,6 +95,10 @@ class TempleteGenerator():
         except ValueError:
             return False
 
+
+    ## Replace contet from a Xls
+    ## Params: documentName = name of the document to replace and generate all the documents
+    ## Returns: none
     def replaceContentXls(self ,documentName ):
 
         for i in range(self.data_count):
@@ -110,7 +137,9 @@ class TempleteGenerator():
             document.save(self.path  + "\\"+ file_name + "_" + str(i) + file_extension)
         
 
-
+    ## Replace contet from a docx
+    ## Params: documentName = name of the document to replace and generate all the documents
+    ## Returns: none
     def replaceContentDoc(self , documentName ):
 
         for i in range(self.data_count):
